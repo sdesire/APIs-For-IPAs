cead9f367e9f2bebf638b8ca58330fe8d1bfc57b
Julio Rios <rios.julio.david@outlook.com>
2021-09-30 21:08:50 -0400

    brewery filetered by type

--- project.js
+++ project.js
@@ -1,11 +1,36 @@
+const breweryType = ["micro", "planning", "nano"];
+
+function getBreweriesTypes () {
+
+}
+
+
 function getBreweries () {
 console.log("Button Clicked");
-  fetch(`https://api.openbrewerydb.org/breweries?by_state=${cityChoice}&sort=${breweryType},name:${ascDesc}`)
+
+const cityChoice = document.getElementById('cityChoice').value;
+
+  fetch(`https://api.openbrewerydb.org/breweries?by_city=${cityChoice}`)
     .then((response) => {
       return response.json();
     })
     .then((response) => {
-      console.log(response);
+      const breweries = response;
+      var filteredResponse = [];
+
+      // for each brewery
+      for (let i = 0; i < breweries.length; i++) {
+
+        //for each brewerie type
+        for (let b = 0; b < breweryType.length; b++) {
+          if(breweries[i].brewery_type === breweryType[b]){
+            filteredResponse.push(breweries[i]);
+            console.log(breweries[i].brewery_type);
+          }   
+        }
+        
+      }
+      console.log(breweries);
     })
     .catch((err) => {
       console.log(err);
