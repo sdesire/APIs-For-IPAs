bd0d93f33e3a75ad00511d32c9432875a5288391
Julio Rios <rios.julio.david@outlook.com>
2021-10-02 18:53:42 -0400

    filtered breweries

--- project.js
+++ project.js
@@ -1,7 +1,34 @@
-const breweryType = ["micro", "planning", "nano"];
-
 function getBreweriesTypes () {
+  var inputDiv = document.getElementById('breweryType');
+  var inputs = inputDiv.getElementsByTagName('input');
 
+  var breweryTypes = [];
+
+  for (let i = 0; i < inputs.length; i++) {
+
+      if(inputs[i].checked){
+        breweryTypes.push(inputs[i].value);
+      } 
+  }
+  return breweryTypes;
+}
+
+function getFilteredBreweries(breweries, breweryTypes){
+  var filteredResponse = [];
+
+    // for each brewery
+    for (let i = 0; i < breweries.length; i++) {
+
+      //for each brewerie type
+      for (let b = 0; b < breweryTypes.length; b++) {
+        if(breweries[i].brewery_type == breweryTypes[b]){
+          filteredResponse.push(breweries[i]);
+          break;
+        }   
+      } 
+    }
+
+    return filteredResponse;
 }
 
 
@@ -9,28 +36,18 @@ function getBreweries () {
 console.log("Button Clicked");
 
 const cityChoice = document.getElementById('cityChoice').value;
+  const breweryTypes = getBreweriesTypes();
 
-  fetch(`https://api.openbrewerydb.org/breweries?by_city=${cityChoice}`)
+  fetch(`https://api.openbrewerydb.org/breweries?by_city=${cityChoice}&per_page=50`)
     .then((response) => {
       return response.json();
     })
     .then((response) => {
       const breweries = response;
-      var filteredResponse = [];
+      const filteredResponse = getFilteredBreweries(breweries, breweryTypes);
 
-      // for each brewery
-      for (let i = 0; i < breweries.length; i++) {
-
-        //for each brewerie type
-        for (let b = 0; b < breweryType.length; b++) {
-          if(breweries[i].brewery_type === breweryType[b]){
-            filteredResponse.push(breweries[i]);
-            console.log(breweries[i].brewery_type);
-          }   
-        }
-        
-      }
-      console.log(breweries);
+      console.log(filteredResponse);
+      return filteredResponse;
     })
     .catch((err) => {
       console.log(err);
