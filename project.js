51f4c4942cf526ce13ef99c0b7ebd6fc503908a2
z0mbiebrad <Bradleyt.evans@gmail.com>
2021-10-04 14:05:44 -0400

    made the brewery websites into hyperlinks so users can easily find out more information

--- project.js
+++ project.js
@@ -1,48 +1,48 @@
-//['micro', 'nano', 'regional', 'brewpub', 'large', 'planning', 'bar', 'contract', 'proprietor'])
 
+  var typesB = ['micro', 'nano', 'regional', 'brewpub', 'large', 'planning', 'bar', 'contract', 'proprietor']
 
-function getBreweriesTypes () {
+function getBreweriesTypes() {
   var inputDiv = document.getElementById('breweryType');
   var inputs = inputDiv.getElementsByTagName('input');
-
   var breweryTypes = [];
 
   for (let i = 0; i < inputs.length; i++) {
 
-      if(inputs[i].checked){
-        breweryTypes.push(inputs[i].value);
-      } 
+    if (inputs[i].checked) {
+      breweryTypes.push(inputs[i].value);
+    }
   }
   return breweryTypes;
   //add option that if none are selected all are displayed**
 }
 
-function getFilteredBreweries(breweries, breweryTypes){
+function getFilteredBreweries(breweries, breweryTypes) {
   var filteredResponse = [];
 
-    // for each brewery
-    for (let i = 0; i < breweries.length; i++) {
+  // for each brewery
+  for (let i = 0; i < breweries.length; i++) {
 
-      //for each brewerie type
-      for (let b = 0; b < breweryTypes.length; b++) {
-        if(breweries[i].brewery_type == breweryTypes[b]){
-          filteredResponse.push(breweries[i]);
-          break;
-        }   
-      } 
+    //for each brewerie type
+    for (let b = 0; b < breweryTypes.length; b++) {
+      if (breweries[i].brewery_type == breweryTypes[b]) {
+        filteredResponse.push(breweries[i]);
+        break;
+      }
     }
+  }
 
-    return filteredResponse;
+  return filteredResponse;
 }
 
 
-function getBreweries () {
-console.log("Button Clicked");
+function getBreweries() {
+  console.log("Button Clicked");
 
-const cityChoice = document.getElementById('cityChoice').value;
+  const cityChoice = document.getElementById('cityChoice').value;
+  const stateChoice = document.getElementById('stateChoice').value;
   const breweryTypes = getBreweriesTypes();
 
-  fetch(`https://api.openbrewerydb.org/breweries?by_city=${cityChoice}&per_page=50`)
+  fetch(`https://api.openbrewerydb.org/breweries?by_city=${cityChoice}&by_state=${stateChoice}&per_page=50,name:asc`)
     .then((response) => response.json())
     .then((response) => {
       const breweries = response;
@@ -51,34 +51,42 @@ const cityChoice = document.getElementById('cityChoice').value;
       const breweryListEl = document.querySelector('#breweryList');
       breweryListEl.innerHTML = '';
       console.log(filteredResponse);
-      filteredResponse.forEach(function(element) {
-      var bName = document.createElement('p');
-      var bType = document.createElement('p');
-      var bStreet = document.createElement('p');
-      var bCity = document.createElement('p');
-      var bState = document.createElement('p');
-      var bPhone = document.createElement('p');
-      var bWebsite = document.createElement('p');
 
-      bName.style.color = "Red"
-      bWebsite.style.margin = "0px auto 10px auto"
-  
-      bName.innerHTML = element.name;
-      bType.innerHTML = element.brewery_type;
-      bStreet.innerHTML = element.street;
-      bCity.innerHTML = element.city;
-      bState.innerHTML = element.state;
-      bPhone.innerHTML = element.phone;
-      bWebsite.innerHTML = element.website_url;
-  
-      breweryListEl.appendChild(bName);
-      breweryListEl.appendChild(bType);
-      breweryListEl.appendChild(bStreet);
-      breweryListEl.appendChild(bCity);
-      breweryListEl.appendChild(bState);
-      breweryListEl.appendChild(bPhone);
-      breweryListEl.appendChild(bWebsite);
-     });
+      //Creates a new element for each attribute we select
+      filteredResponse.forEach(function (element) {
+        var bName = document.createElement('p');
+        var bType = document.createElement('p');
+        var bStreet = document.createElement('p');
+        var bCity = document.createElement('p');
+        var bState = document.createElement('p');
+        var bWebsite = document.createElement('a');
+        var bPhone = document.createElement('p');
+
+        //Style
+        bName.style.color = "Red"
+        bPhone.style.margin = "0px auto 10px auto"
+        bWebsite.setAttribute("href", element.website_url)
+
+        //Inserts data from the JSON
+        bName.innerHTML = element.name;
+        bType.innerHTML = element.brewery_type;
+        bStreet.innerHTML = element.street;
+        bCity.innerHTML = element.city;
+        bState.innerHTML = element.state;
+        bWebsite.innerHTML = element.website_url;
+        bPhone.innerHTML = element.phone;
+
+        //appends each element to contain the inserted Data        
+        breweryListEl.appendChild(bName);
+        breweryListEl.appendChild(bType);
+        breweryListEl.appendChild(bStreet);
+        breweryListEl.appendChild(bCity);
+        breweryListEl.appendChild(bState);
+        breweryListEl.appendChild(bWebsite);
+        breweryListEl.appendChild(bPhone);
+
+        return filteredResponse
+      });
 
     })
     .catch((err) => {
