0b9a949d696b56a3445b39b897afac47cae1d8ba
Jared Gavigan <jagavigan80@gmail.com>
2021-10-06 19:30:38 -0400

    icon-installed

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var desc = document.querySelector('#desc');
 var clouds = document.querySelector('#clouds');
 var button = document.querySelector('#btn');
 var hum = document.querySelector('#humidity')
+var icon = document.querySelector('.icon');
 
 button.addEventListener('click', function (){
 fetch('http://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=imperial&appid=50a7aa80fa492fa92e874d23ad061374')
@@ -15,6 +16,8 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=im
   var descValue = data['weather'][0]['description'];
   var humValue  = data['main']['humidity'];
   var descCap = descValue.split(" ");
+  var iconSrc  = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+
   for (var i = 0; i < descCap.length; i++) {
     descCap[i] = descCap[i].charAt(0).toUpperCase() + descCap[i].slice(1);
 }
@@ -26,6 +29,7 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=im
   temp.innerHTML = "Temperature: " + tempValue + "&#176;";
   hum.innerHTML = "Humidity: " + humValue + "&#37;";
   input.value ="";
+  icon.setAttribute("Src", iconSrc);
 
 })
 
