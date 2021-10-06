00a09fd96597e5992a7ada21176c7b28a5953fad
z0mbiebrad <Bradleyt.evans@gmail.com>
2021-10-06 16:26:06 -0400

    changed weather setup and cleaned up the look

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var input = document.querySelector('#cityChoice');
 var main = document.querySelector('#name');
-var temp = document.querySelector('.temp');
-var desc = document.querySelector('.desc');
-var clouds = document.querySelector('.clouds');
+var temp = document.querySelector('#temp');
+var desc = document.querySelector('#desc');
+var clouds = document.querySelector('#clouds');
 var button = document.querySelector('#btn');
+var hum = document.querySelector('#humidity')
 
 button.addEventListener('click', function (){
 fetch('http://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=imperial&appid=50a7aa80fa492fa92e874d23ad061374')
@@ -12,10 +13,18 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=im
   var tempValue = Math.floor (data['main']['temp']);
   var nameValue = data['name'];
   var descValue = data['weather'][0]['description'];
+  var humValue  = data['main']['humidity'];
+  var descCap = descValue.split(" ");
+  for (var i = 0; i < descCap.length; i++) {
+    descCap[i] = descCap[i].charAt(0).toUpperCase() + descCap[i].slice(1);
+}
+    var descCap2 = descCap.join(" ");
+    console.log(descCap2);
 
   main.innerHTML = nameValue;
-  desc.innerHTML = "Desc - "+descValue;
-  temp.innerHTML = "Temp - "+tempValue;
+  desc.innerHTML = "Weather: " + descCap2;
+  temp.innerHTML = "Temperature: " + tempValue + "&#176;";
+  hum.innerHTML = "Humidity: " + humValue + "&#37;";
   input.value ="";
 
 })
