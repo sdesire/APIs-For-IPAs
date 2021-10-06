8f8d1e9a5ff2cc77e63d8c0667564e994dd72366
Jared Gavigan <jagavigan80@gmail.com>
2021-10-06 19:03:34 -0400

    icons-added-to

--- script.js
+++ script.js
@@ -4,31 +4,53 @@ var temp = document.querySelector('.temp');
 var desc = document.querySelector('.desc');
 var clouds = document.querySelector('.clouds');
 var button = document.querySelector('.submit');
+var icon = document.querySelector('.icon');
 
 
 button.addEventListener('click', function(name){
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=imperial&appid=50a7aa80fa492fa92e874d23ad061374')
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=imperial&appid=f6b36b81de024b7633ed8e427463edeb')
 
 .then(response => response.json())
 .then(data => {
+  //var forecast = data['list'];
+  //var futureWeather = forecast[1]['main']['temp']
   var tempValue = Math.floor (data['main']['temp']);
   var nameValue = data['name'];
   var descValue = data['weather'][0]['description'];
- // var icon = "http://openweathermap.org/img/wn/10d@2x.png" + data.weather[0].icon + ".png";
+  var iconSrc  = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
   
-//$('.icon').attr("src", icon);
-//$('weather').append(weather);
-//$('.temp').append(temp);
   
+/*for (var counter = 0; counter < forecast.length; counter++) {
+  if(counter < 1){
+    console.log('Inside the loop:' + counter);
+    console.log('Inside the list:' + forecast[counter]);
+    var icon = document.querySelector('.icon' + counter);
+    var date = document.querySelector('.date' + counter);
+    var temp = document.querySelector('.temp' + counter);
+    var desc = document.querySelector('.desc' + counter);
+
+    var futureWeather = forecast[counter]['main']['temp'];
+    var descValue = forecast[counter]['weather'][0]['description'];
+    var iconSrc  = "http://openweathermap.org/img/wn/" + forecast[counter]['weather'][0]['icon'] + ".png";
+    var datevalue = forecast[counter]['dt_txt'];
+    desc.innerHTML = descValue;
+    temp.innerHTML = "Temp - "+futureWeather;
+    icon.setAttribute("Src", iconSrc);
+    date.innerHTML = datevalue;
+  }
+}*/
+
+
   main.innerHTML = nameValue;
-  desc.innerHTML = "Desc - "+descValue;
+  desc.innerHTML = descValue;
   temp.innerHTML = "Temp - "+tempValue;
   input.value ="";
+  icon.setAttribute("Src", iconSrc);
   
 
 })
 
-.catch(err => alert("Wrong city name!"));
+.catch(err => alert("Wrong city name!"+err));
 })
 
 /*catch error*/
